abd13e27b5603acbdbfe5ccec8910cfc7d2a95c2
aastha_abhani_18 <abhaniaastha18@gmail.com>
2026-07-25 19:42:32 +0530

    Add files via upload

---
+++ project2 logic box.py
@@ -0,0 +1,40 @@
+#logic box:
+print("-"*40)
+print("logic box project"
+      )
+print("-"*40)
+
+print("-"*40)
+print("pattern generator & number analzer")
+print("-"*40)
+
+choice=input("enter your number")
+if choice =="1":
+ rows =int(input("enter number of row"))
+
+if rows==1:
+           print("invalid row count !")
+
+for i in range (1,rows+1):
+ for i in range (i):
+    print("*",end ="")
+ print(range)
+
+if choice =="2":
+       start=int (input("enter start number"))
+end=int  (input("enter end number"))
+
+if start > end:
+    print("Invalid number")
+
+print ("num analzer")
+for num in range,end+1:
+  print (choice)
+
+if num % 2 == 0:
+
+      print(number,"is even")
+      print(number,"is odd")
+total + sum
+print("Sum of all numbers =", total)
+
